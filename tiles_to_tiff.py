83f7263fbde593a8448ab8d6b3d813967915d76c
jayin92 <jayin920805@gmail.com>
2020-09-29 15:27:04 +0800

    edit some format string bugs

--- tiles_to_tiff.py
+++ tiles_to_tiff.py
@@ -8,15 +8,14 @@ from tile_convert import bbox_to_xyz, tile_edges
 from osgeo import gdal
 
 #---------- CONFIGURATION -----------#
-tile_server = "https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=" + os.environ.get(
-    'MAPBOX_ACCESS_TOKEN')
+tile_server = "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
 temp_dir = os.path.join(os.path.dirname(__file__), 'temp')
 output_dir = os.path.join(os.path.dirname(__file__), 'output')
-zoom = 16
-lon_min = 21.49147
-lon_max = 21.5
-lat_min = 65.31016
-lat_max = 65.31688
+zoom = 12
+lon_min = 121
+lon_max = 122
+lat_min = 24
+lat_max = 25
 #-----------------------------------#
 
 
@@ -25,7 +24,7 @@ def download_tile(x, y, z, tile_server):
         "{x}", str(x)).replace(
         "{y}", str(y)).replace(
         "{z}", str(z))
-    path = f'{temp_dir}/{x}_{y}_{z}.png'
+    path = '{}/{}_{}_{}.png'.format(temp_dir, x, y, z)
     urllib.request.urlretrieve(url, path)
     return(path)
 
@@ -51,18 +50,18 @@ def georeference_raster_tile(x, y, z, path):
 x_min, x_max, y_min, y_max = bbox_to_xyz(
     lon_min, lon_max, lat_min, lat_max, zoom)
 
-print(f"Downloading {(x_max - x_min + 1) * (y_max - y_min + 1)} tiles")
+print("Downloading {} tiles".format((x_max - x_min + 1) * (y_max - y_min + 1)))
 
 for x in range(x_min, x_max + 1):
     for y in range(y_min, y_max + 1):
-        print(f"{x},{y}")
+        print("{},{}".format(x, y))
         png_path = download_tile(x, y, zoom, tile_server)
         georeference_raster_tile(x, y, zoom, png_path)
 
 print("Download complete")
 
 print("Merging tiles")
-merge_tiles(temp_dir + '/*.tif', output_dir + '/merged.tif')
+merge_tiles(temp_dir + '/*.tif', output_dir + '/merged1.tif')
 print("Merge complete")
 
 shutil.rmtree(temp_dir)
