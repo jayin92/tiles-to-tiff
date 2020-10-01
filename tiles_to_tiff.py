cdb9dfc9fe70d93e9e55e90ded2d54505b212ec5
jayin92 <jayin920805@gmail.com>
2020-10-01 12:04:47 +0000

    support different regions

--- tiles_to_tiff.py
+++ tiles_to_tiff.py
@@ -4,27 +4,13 @@ import os
 import glob
 import subprocess
 import shutil
-import requests
+# import requests
 from tqdm import tqdm
 
-from fp.fp import FreeProxy
+# from fp.fp import FreeProxy
 from tile_convert import bbox_to_xyz, tile_edges
 from osgeo import gdal
 
-#---------- CONFIGURATION -----------#
-# tile_server = "https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-tile_server = "https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=AIf4cL2Jkh4Vlf3AylHN"
-temp_dir = os.path.join(os.path.dirname(__file__), 'temp')
-output_dir = os.path.join(os.path.dirname(__file__), 'output/')
-zoom = 13
-
-lat_min = 27
-lon_min = 98
-lon_max = lon_min + 1
-lat_max = lat_min + 1
-# lon_max = 122
-# lat_max = 25
-#-----------------------------------#
 
 
 
@@ -78,7 +64,7 @@ def crop(input, output, lat, lon):
     )
 
 
-ran = [
+china = [
     (26, 101),
     (26, 100),
     (26, 99),
@@ -94,9 +80,73 @@ ran = [
     (27, 100)
 ]
 
+hima = [
+    (29, 81),
+    (29, 82),
+    (29, 83),
+    (28, 83),
+    (28, 84),
+    (28, 85),
+    (27, 85),
+    (27, 86),
+    (28, 86),
+    (27, 87)
+]
 
-for lat_min, lon_min in ran:
+peru = [
+    (-7, -79),
+    (-8, -79),
+    (-8, -78),
+    (-9, -78),
+    (-10, -78),
+    (-11, -77),
+    (-12, -77),
+    (-12, -76),
+    (-13, -76),
+    (-14, -76),
+    (-14, -75),
+    (-15, -75),
+    (-15, -74),
+    (-16, -73),
+]
+
+arge = [
+    (-48, -74),
+    (-49, -74),
+    (-49, -75),
+    (-50, -74),
+    (-50, -75),
+    (-51, -73),
+    (-51, -74),
+    (-51, -75),
+    (-52, -74),
+]
+
+cana = [
+    (58, -134),
+    (57, -133),
+    (56, -132),
+    (56, -131),
+    (55, -131),
+]
+
+#---------- CONFIGURATION -----------#
+# tile_server = "https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+region = "cana"
+tile_server = "https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=AIf4cL2Jkh4Vlf3AylHN"
+temp_dir = os.path.join(os.path.dirname(__file__), 'temp/'+region)
+output_dir = os.path.join(os.path.dirname(__file__), 'output/'+region)
+zoom = 13
+
+# lon_max = 122
+# lat_max = 25
+#-----------------------------------#
+for lat_min, lon_min in cana:
     print(lat_min, lon_min)
+
+    lat_dir = ("N" if lat_min > 0 else "S")
+    lon_dir = ("E" if lon_min > 0 else "W")
+
     lat_max = lat_min + 1
     lon_max = lon_min + 1
     x_min, x_max, y_min, y_max = bbox_to_xyz(
@@ -117,16 +167,17 @@ for lat_min, lon_min in ran:
     print("Download complete")
 
     print("Merging tiles")
-    merge_tiles(temp_dir + '/*.tif', output_dir + '/N{}E{}_{}.tif'.format(lat_min, lon_min, zoom))
+    merge_tiles(temp_dir + '/*.tif', output_dir + '/{}{}{}{}_{}.tif'.format(lat_dir, abs(lat_min), lon_dir, abs(lon_min), zoom))
     print("Merge complete")
 
     print("Croping")
+    
     crop(
-        output_dir + '/N{}E{}_{}.tif'.format(lat_min, lon_min, zoom), 
-        output_dir + 'China/N{}E{}_{}_final_sat.tif'.format(lat_min, lon_min, zoom), 
+        output_dir + '/{}{}{}{}_{}.tif'.format(lat_dir, abs(lat_min), lon_dir, abs(lon_min), zoom), 
+        output_dir + '{}{}{}{}_{}_final_sat.tif'.format(lat_dir, abs(lat_min), lon_dir, abs(lon_min), zoom), 
         lat_min, 
         lon_min
-        )
+    )
     print("Crop complete")
 
 
